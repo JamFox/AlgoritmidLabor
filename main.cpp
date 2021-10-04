d01a3fd807c3e6b12608a6e3fcf861c2b92f2418
katurv <katurv@ttu.ee>
2021-10-04 22:16:56 +0300

    autoformat and del free func

--- main.cpp
+++ main.cpp
@@ -25,119 +25,119 @@ unsigned long int randomlx(int l, int r)
 
 void PrintObjects(HeaderD* pStruct7)
 {
-	// Initialize the traversal pointer
-	HeaderD* pStructTemp = pStruct7;
-	int i = 1, j = 1;
+  // Initialize the traversal pointer
+  HeaderD* pStructTemp = pStruct7;
+  int i = 1, j = 1;
 
-	// Traverse double linked list of headers until we hit NULL on pNext
-	while (pStructTemp != NULL)
-	{ 
+  // Traverse double linked list of headers until we hit NULL on pNext
+  while (pStructTemp != NULL)
+  { 
 
-		// Print header number and it's cBegin
-		printf("[Hdr %d] %c\n", i, pStructTemp->cBegin);
+    // Print header number and it's cBegin
+    printf("[Hdr %d] %c\n", i, pStructTemp->cBegin);
 
-		// Declare a temp pointer from header struct
-		Object4* pObjectTemp = (Object4*)pStructTemp->pObject;
+    // Declare a temp pointer from header struct
+    Object4* pObjectTemp = (Object4*)pStructTemp->pObject;
 
-		// Traverse linked list of objects
-		while (pObjectTemp != NULL)
-		{ 
-	  	// Print Object ID, Code and Date
-			printf("\t (Obj %d) %s ", j, pObjectTemp->pID); 
-			printf("%lu %s\n", pObjectTemp->Code, pObjectTemp->pDate);
-			// Set traversal pointer to pNext of current Object
-			pObjectTemp = pObjectTemp->pNext;
+    // Traverse linked list of objects
+    while (pObjectTemp != NULL)
+    { 
+      // Print Object ID, Code and Date
+      printf("\t (Obj %d) %s ", j, pObjectTemp->pID); 
+      printf("%lu %s\n", pObjectTemp->Code, pObjectTemp->pDate);
+      // Set traversal pointer to pNext of current Object
+      pObjectTemp = pObjectTemp->pNext;
       j++;
-		}
+    }
 
-		// Set traversal pointer to pNext of current Header
-		pStructTemp = pStructTemp->pNext;
-		i++;
-	}
+    // Set traversal pointer to pNext of current Header
+    pStructTemp = pStructTemp->pNext;
+    i++;
+  }
 }
 
 HeaderD* pHeaderDNew(char* pNewID, HeaderD* pStructPrior, HeaderD* pStructNext)
 {
-	HeaderD* pStructNew = (HeaderD*)malloc(sizeof(HeaderD));
-	if (pStructNew == NULL)
-	{
-		printf("InsertNewObject(): malloc failed!");
-		exit(EXIT_FAILURE);
-	}
-	pStructNew->pPrior = pStructPrior;
-	pStructNew->pNext = pStructNext;
-	pStructNew->cBegin = *pNewID;
-	pStructNew->pObject = NULL;
-	// Change pointers to insert new Header into list
-	if (pStructNext != NULL)
-	{
-		pStructNext->pPrior = pStructNew;
-	}
-	return pStructNew;
+  HeaderD* pStructNew = (HeaderD*)malloc(sizeof(HeaderD));
+  if (pStructNew == NULL)
+  {
+    printf("InsertNewObject(): malloc failed!");
+    exit(EXIT_FAILURE);
+  }
+  pStructNew->pPrior = pStructPrior;
+  pStructNew->pNext = pStructNext;
+  pStructNew->cBegin = *pNewID;
+  pStructNew->pObject = NULL;
+  // Change pointers to insert new Header into list
+  if (pStructNext != NULL)
+  {
+    pStructNext->pPrior = pStructNew;
+  }
+  return pStructNew;
 }
 
 Object4* pObject4New(char* pNewID, unsigned long int NewCode, Object4* pNextOb) {
-	// Allocate memory for new Object
-	Object4* pObjectNew = (Object4*)malloc(sizeof(Object4));
-	if (pObjectNew == NULL)
-	{
-		printf("InsertNewObject(): malloc failed!");
-		exit(EXIT_FAILURE);
-	}
-	// Insert Code to new Object
-	pObjectNew->Code = NewCode;
-	// Insert Date to new Object
-	time_t RawTime = time(NULL);
-	char* pResult = (char*)malloc(DATESTRINGLEN);
-	if (pResult == NULL)
-	{
-		printf("InsertNewObject(): malloc failed!");
-		exit(EXIT_FAILURE);
-	}
-	if (GetDateString(RawTime, DATESTRINGLEN, pResult) == 1)
-	{
-		pObjectNew->pDate = pResult;
-	}
-	else {
-		printf("InsertNewObject(): Unable to get date!");
-		exit(EXIT_FAILURE);
-	}
-	// Allocate memory for new ID and insert into new Object
-	pObjectNew->pID = (char*)malloc(strlen(pNewID));
-	strcpy(pObjectNew->pID, pNewID);
-	// Set next in list 
-	pObjectNew->pNext = pNextOb;
-	return pObjectNew;
+  // Allocate memory for new Object
+  Object4* pObjectNew = (Object4*)malloc(sizeof(Object4));
+  if (pObjectNew == NULL)
+  {
+    printf("InsertNewObject(): malloc failed!");
+    exit(EXIT_FAILURE);
+  }
+  // Insert Code to new Object
+  pObjectNew->Code = NewCode;
+  // Insert Date to new Object
+  time_t RawTime = time(NULL);
+  char* pResult = (char*)malloc(DATESTRINGLEN);
+  if (pResult == NULL)
+  {
+    printf("InsertNewObject(): malloc failed!");
+    exit(EXIT_FAILURE);
+  }
+  if (GetDateString(RawTime, DATESTRINGLEN, pResult) == 1)
+  {
+    pObjectNew->pDate = pResult;
+  }
+  else {
+    printf("InsertNewObject(): Unable to get date!");
+    exit(EXIT_FAILURE);
+  }
+  // Allocate memory for new ID and insert into new Object
+  pObjectNew->pID = (char*)malloc(strlen(pNewID));
+  strcpy(pObjectNew->pID, pNewID);
+  // Set next in list 
+  pObjectNew->pNext = pNextOb;
+  return pObjectNew;
 }
 
 bool isLetters(char* pInput)
 {
-	while (*pInput != '\0')
-	{
-		int uppercaseChar = toupper(*pInput);
-		if (uppercaseChar < 'A' || uppercaseChar > 'Z') 
-		{
+  while (*pInput != '\0')
+  {
+    int uppercaseChar = toupper(*pInput);
+    if (uppercaseChar < 'A' || uppercaseChar > 'Z') 
+    {
       printf("String is not composed of letters!\n");
-			return false;
-		}
-		pInput++;
-	}
-	return true; 
+      return false;
+    }
+    pInput++;
+  }
+  return true; 
 }
 
 bool lowerAfterFirst(char* pInput)
 {
   pInput++;
-	while (*pInput != '\0')
-	{
-		if (isupper(*pInput)) 
-		{
+  while (*pInput != '\0')
+  {
+    if (isupper(*pInput)) 
+    {
       printf("Letters after first are not lowercase!\n");
-			return false;
-		}
-		pInput++;
-	}
-	return true; 
+      return false;
+    }
+    pInput++;
+  }
+  return true; 
 }
 
 
@@ -145,29 +145,29 @@ int InsertNewObject(HeaderD** pStruct7, char* pNewID, int NewCode)
 {
   // Print error if ID is not with first upper letter, does not consist...
   // ...fully of letters or is not lowercase after first upper letter
-	if (!isupper(*pNewID) || !isLetters(pNewID) || !lowerAfterFirst(pNewID))
-	{
-		printf("ID does not match formatting requirements!\n");
-		return 0;
-	}
-	// Initialize the traversal pointer 
-	HeaderD* pStructTemp = *pStruct7;
+  if (!isupper(*pNewID) || !isLetters(pNewID) || !lowerAfterFirst(pNewID))
+  {
+    printf("ID does not match formatting requirements!\n");
+    return 0;
+  }
+  // Initialize the traversal pointer 
+  HeaderD* pStructTemp = *pStruct7;
 
   bool firstloop = true;
-	while (*pNewID != pStructTemp->cBegin)
-	{
-		// If next pointer is NULL...
-		// ...then create new Header for Object
-		if (pStructTemp->pNext == NULL)
-		{
-			pStructTemp->pNext = pHeaderDNew(pNewID, pStructTemp, pStructTemp->pNext);
-			pStructTemp = pStructTemp->pNext;
-			break;
+  while (*pNewID != pStructTemp->cBegin)
+  {
+    // If next pointer is NULL...
+    // ...then create new Header for Object
+    if (pStructTemp->pNext == NULL)
+    {
+      pStructTemp->pNext = pHeaderDNew(pNewID, pStructTemp, pStructTemp->pNext);
+      pStructTemp = pStructTemp->pNext;
+      break;
     }            
-		// If cBegin of next Header is later in...
-		// ...alphabet, create a new Header
-	  if (pStructTemp->pNext->cBegin > *pNewID)
-		{
+    // If cBegin of next Header is later in...
+    // ...alphabet, create a new Header
+    if (pStructTemp->pNext->cBegin > *pNewID)
+    {
       // If it's the first loop then there is no pPrior 
       if(firstloop == true) 
       {
@@ -175,22 +175,22 @@ int InsertNewObject(HeaderD** pStruct7, char* pNewID, int NewCode)
         pStructTemp = *pStruct7;
         break;
       }
-			pStructTemp->pNext = pHeaderDNew(pNewID, pStructTemp, pStructTemp->pNext);
+      pStructTemp->pNext = pHeaderDNew(pNewID, pStructTemp, pStructTemp->pNext);
       pStructTemp = pStructTemp->pNext;
-			break;
-		}
-		// Set traversal pointer to pNext of current Header
-		pStructTemp = pStructTemp->pNext;
+      break;
+    }
+    // Set traversal pointer to pNext of current Header
+    pStructTemp = pStructTemp->pNext;
     firstloop = false;
-	}
+  }
 
-	// Declare traversal pointers
-	Object4* pObjectTemp = (Object4*)pStructTemp->pObject;
+  // Declare traversal pointers
+  Object4* pObjectTemp = (Object4*)pStructTemp->pObject;
   firstloop = true; 
   Object4* pObjectPrevious = pObjectTemp;
   int cmpvalue;
   // If Object ptr is not NULL, then loop through Objects to fins insertion point
-	for(;pObjectTemp != NULL;
+  for(;pObjectTemp != NULL;
       pObjectPrevious=pObjectTemp,pObjectTemp = pObjectTemp->pNext)
   {
     cmpvalue = strcmp(pNewID, pObjectTemp->pID);
@@ -219,16 +219,16 @@ int InsertNewObject(HeaderD** pStruct7, char* pNewID, int NewCode)
     }
   }
   // Add object as the first in Headers list
-	pStructTemp->pObject = pObject4New(pNewID, NewCode, pObjectTemp);
-	return 1;
+  pStructTemp->pObject = pObject4New(pNewID, NewCode, pObjectTemp);
+  return 1;
 }
 
 Object4* RemoveExistingObject(HeaderD **pStruct7, char *pExistingID)
 {
-	HeaderD* pStructTemp = *pStruct7;
+  HeaderD* pStructTemp = *pStruct7;
 
-	while (*pExistingID != pStructTemp->cBegin)
-	{
+  while (*pExistingID != pStructTemp->cBegin)
+  {
 
   }
   Object4* pObjectTemp = (Object4*)pStructTemp->pObject; 
@@ -237,57 +237,28 @@ Object4* RemoveExistingObject(HeaderD **pStruct7, char *pExistingID)
 
 int main()
 {
-	// Struct is given by instructor, for Karl-Andreas Turvas Struct7
-	// g) Struct7 puhul: 
-	HeaderD* pStruct7 = GetStruct7(O, N);
-	PrintObjects(pStruct7);
+  // Struct is given by instructor, for Karl-Andreas Turvas Struct7
+  // g) Struct7 puhul: 
+  HeaderD* pStruct7 = GetStruct7(O, N);
+  PrintObjects(pStruct7);
 
   // Create strings for tests as specified in lab instructions
   const char *strings[] = {"Dx","Db","Dz","Dk","Aa","Wu","Wa","Zw","Za","wk","Wa","WW","W8","W_"};
 
   // Loop through strings and add them to the appropriate lists
-	HeaderD** ppStruct7 = &pStruct7;
+  HeaderD** ppStruct7 = &pStruct7;
   for (int i = 0; i < 14; i++){
-	  if (InsertNewObject(ppStruct7, (char*)strings[i], randomlx(0, 999999)) == 0) {
+    if (InsertNewObject(ppStruct7, (char*)strings[i], randomlx(0, 999999)) == 0) {
       printf("InsertNewObject(): failed! Could not add %s \n\n", strings[i]);
-	  }
+    }
   }
-	PrintObjects(pStruct7);
+  PrintObjects(pStruct7);
 
   // Loop through strings and remove them from lists
   for (int i = 0; i < 14; i++){
-  
+
   }
-	PrintObjects(pStruct7);
+  PrintObjects(pStruct7);
 
-	return 0;
+  return 0;
 }
-
-/*
-// Unnecessary free function for generated struct
-void freeStruct(HeaderD* pStruct7)
-{
-	HeaderD* pStructTemp;
-	int i, j;
-
-	// Traverse double linked list of headers and free them until we hit NULL on pNext
-	while (pStruct7 != NULL)
-	{
-
-		// Free Objects
-		while (pStruct7->pObject != NULL)
-		{
-			// Declare a temp pointer from header struct
-			Object4* pObjectTemp = (Object4*)pStruct7->pObject;
-			pStruct7->pObject = pObjectTemp->pNext;
-			free(pObjectTemp);
-		}
-
-		pStructTemp = pStruct7;
-		pStruct7 = pStruct7->pNext;
-		free(pStructTemp);
-	}
-
-}
-*/
-
